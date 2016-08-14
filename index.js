e0c157eee5b32a2c4119989b2638fbcb212b22b2
Janelle <janelle.evan@gmail.com>
2016-08-14 18:59:14 +0000

    jira implementation

--- index.js
+++ index.js
@@ -88,30 +88,48 @@ controller.setupWebserver(process.env.PORT, function (err, webserver) {
 controller.on('slash_command', function (slashCommand, message) {
 
     switch (message.command) {
-        case "/echo": //handle the `/echo` slash command. We might have others assigned to this app too!
+        case "/jira": //handle the `/echo` slash command. We might have others assigned to this app too!
             // The rules are simple: If there is no text following the command, treat it as though they had requested "help"
             // Otherwise just echo back to them what they sent us.
 
             // but first, let's make sure the token matches!
-            if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
+            // if (message.token !== process.env.VERIFICATION_TOKEN) return; //just ignore it.
 
             // if no text was supplied, treat it as a help command
             if (message.text === "" || message.text === "help") {
                 slashCommand.replyPrivate(message,
-                    "I echo back what you tell me. " +
-                    "Try typing `/echo hello` to see.");
+                    "I help you look up JIRA templates. " +
+                    "Try typing `/jira change events` to see.");
+                return;
+            }
+            
+            switch(message.text){
+                case "change events":
+                    t = "*Context:* [What is happening?]\n" + 
+"*Scope:*\n" + 
+"*Has this been tested on multiple browsers? [Yes/No - Which browsers]*: ";
+                    slashCommand.replyPrivate(message, t);
+                return;
+                case "submittals":
+                    
+                    return;
+                case "other":
+                    
+                    return;
+                default:
+                slashCommand.replyPrivate(message, "I don't under stand that");
                 return;
             }
 
             // If we made it here, just echo what the user typed back at them
             //TODO You do it!
-            slashCommand.replyPublic(message, "1", function() {
+            slashCommand.replyPrivate(message, "1", function() {
                 slashCommand.replyPublicDelayed(message, "2").then(slashCommand.replyPublicDelayed(message, "3"));
             });
 
             break;
         default:
-            slashCommand.replyPublic(message, "I'm afraid I don't know how to " + message.command + " yet.");
+            slashCommand.replyPrivate(message, "I'm afraid I don't know how to " + message.command + " yet.");
 
     }
 
