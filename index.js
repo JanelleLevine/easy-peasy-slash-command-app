0f422fd0ddd05d590ad391ec95a4671b870ed164
JanelleLevine <janelle.evan@gmail.com>
2016-08-14 22:11:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -296,7 +296,7 @@ controller.on('slash_command', function (slashCommand, message) {
                     slashCommand.replyPrivate(message, t);
                     return;
                     case "auto file ticket":
-                    t = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+                    t = "Auto File Link: http://bit.ly/IqT6zt";
                     slashCommand.replyPrivate(message, t);
                     return;
                 default:
